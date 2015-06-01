22b32a4c633c2978b28e91ad2acc66d15ac8b5df
Ceytix <ceytix@mailoo.org>
2015-06-01 17:10:41 +0200

    better replay function

---
+++ src/functions.cpp
@@ -0,0 +1,38 @@
+/**
+ * ========================
+ * 
+ * Filename: functions.cpp
+ * 
+ * Author: Ceytix <ceytix@mailoo.org>
+ * 
+ * Description: Functions which make the code more attractive
+ * 
+ * Created: 01/06/15
+ * =========================
+ **/
+ 
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+bool playAgain()
+{
+	char replay (' ');
+	
+	cout << "Do you want to play again ? yes(y) / no(n)" << endl;
+		
+	while(replay!='y' && replay!='n')
+	{
+		cin >> replay;
+	}
+	
+	if(replay == 'y')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
---
+++ src/functions.h
@@ -0,0 +1,15 @@
+/**
+ * ========================
+ * 
+ * Filename: functions.h
+ * 
+ * Author: Ceytix <ceytix@mailoo.org>
+ * 
+ * Description: Functions which make the code more attractive
+ * 
+ * Created: 01/06/15
+ * =========================
+ **/
+ 
+
+bool playAgain();
